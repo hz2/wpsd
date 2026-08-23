310cb76d68f90cd164bd4e5dda229602a1b3f0b3
hz2 <dev.json2@gmail.com>
2026-08-22 20:20:40 -0700

    chore: bump version to 0.1.4

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
 
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "wpsd";
-          version = "0.1.3";
+          version = "0.1.4";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
         };
